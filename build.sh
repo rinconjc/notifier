94900e325dc56b88fec6e867304d0b28a0625b98
Julio <jcrinconc@gmail.com>
2015-12-03 07:35:09 +1100

    added build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# install lein
+
+if [[ ! -f ~/bin/lein ]]; then
+    echo "lein not present! downloading ..."
+    mkdir -p ~/bin
+    wget --no-check-certificate  -O ~/bin/lein "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein"
+    chmod 755 ~/bin/lein
+fi
+
+~/bin/lein $1
