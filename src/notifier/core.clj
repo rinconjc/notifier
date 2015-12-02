32cf567176db18c2fd2612b617a9fa6a64f974b4
Julio <jcrinconc@gmail.com>
2015-12-03 07:27:21 +1100

    ready

--- src/notifier/core.clj
+++ src/notifier/core.clj
@@ -1,12 +1,15 @@
 (ns notifier.core
   (:import [javax.crypto Mac]
            [javax.crypto.spec SecretKeySpec]
-           [java.util Base64]
-           [org.httpkit.client :as http])
-  (:require [clojure.data.json :as json])
+           [java.util Base64])
+  (:require [clojure.data.json :as json]
+            [clj-time.core :as t]
+            [chime :refer [chime-at]]
+            [clj-time.periodic :refer [periodic-seq]]
+            [org.httpkit.client :as http])
   (:gen-class))
 
-(def last-price (atom nil))
+(def ifttt-key (delay (System/getProperty "ifttt-key")))
 
 (defn sign-req [uri key body]
   (let [mac (doto (Mac/getInstance "HmacSHA512")
@@ -20,22 +23,37 @@
   (let [{:keys [status headers body error] :as resp} @(http/get "https://api.btcmarkets.net/market/BTC/AUD/tick")
         data (json/read-str body)
         last-price (data "lastPrice")]
+    (println "retrieved btc price:" status ", error:" error ", price:" last-price)
     (if error (println "failed with " error)
         last-price)))
 
+(defn publish-event [level price percent]
+  (let[url (str "https://maker.ifttt.com/trigger/BTC-AUD-%s/with/key/%" level @ifttt-key)
+       {:keys[status body]} (http/post url {:headers {"Content-Type:" "application/json"}
+                                            :body (json/write-str {:value1 price :value2 percent})})]
+    (println "published event:" url " result:" status ":" body)))
+
 (defn mk-price-nofifier [changes]
-  (let[last-prices (atom (into {} (for [c changes] [c (atom nil)])))]
+  (let[last-prices (atom {})]
     (fn[]
       (if-let [price (get-btc-price)]
         (doseq [c changes :let [last-price (@last-prices c)]]
-          (if @last-price
-            (let [change (@last-price (- price @last-price))]
-              (when (> change c)
-                (notify-change c price (/ change @last-price))
-                (assoc! )))
-            (reset! last-price price)))))))
+          (if-let [change (and last-price (- price last-price))]
+            (when (>= (Math/abs change) c)
+              (publish-event c price (/ change last-price))
+              (swap! last-prices assoc c price))
+            (swap! last-prices assoc c price)))))))
+
+(defn schedule []
+  (let [notifier (mk-price-nofifier [10 20 30 40 50 100])
+        times (periodic-seq (t/now) (-> 2 t/minutes))]
+    (chime-at times (fn[time]
+                      (println "chiming at " time)
+                      (notifier)))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Starting notifier..."
   [& args]
-  (println "Hello, World!"))
+  (println "Starting notifier..")
+  (schedule)
+  (Thread/sleep (Long/MAX_VALUE)))
