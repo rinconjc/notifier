304b76b3f96e1c576194d4b94666a0fb206a0b5a
Julio <jcrinconc@gmail.com>
2015-12-05 07:46:54 +1100

    log hooks

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 
 # git hook to deploy code
 
-cd ~/notifier
-git pull
-pkill -f lein
-ifttt=$(cat .key)
-nohup env JVM_OPTS="-Difttt-key=$ifttt" ./build.sh run &
+$(
+    cd ~/notifier
+    git pull
+    pkill -f lein
+    ifttt=$(cat .key)
+    nohup env JVM_OPTS="-Difttt-key=$ifttt" ./build.sh run &
+) >> /var/tmp/git-hook.log
