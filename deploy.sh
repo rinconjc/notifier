0b0dad25cc62234fad2ba3782fe91df88663e25a
Julio <jcrinconc@gmail.com>
2015-12-05 07:54:23 +1100

    updated deployer

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,8 @@
 
 # git hook to deploy code
 
-$(
-    cd ~/notifier
-    git pull
-    pkill -f lein
-    ifttt=$(cat .key)
-    nohup env JVM_OPTS="-Difttt-key=$ifttt" ./build.sh run &
-) >> /var/tmp/git-hook.log
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+git pull
+pkill -f lein
+ifttt=$(cat .key)
+nohup env JVM_OPTS="-Difttt-key=$ifttt" ./build.sh run &
