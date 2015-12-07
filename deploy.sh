0f5657f8139b87f7cff56e12dce48630830652ed
Julio <jcrinconc@gmail.com>
2015-12-07 12:29:35 +1100

    ignore missing process

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "cur dir: $PWD"
 git pull
 
-pkill -f lein
+pkill -f lein | echo
 ifttt=$(cat .key)
 env JVM_OPTS="-Difttt-key=$ifttt" ./build.sh run > ./run.log 2>&1 &
 
